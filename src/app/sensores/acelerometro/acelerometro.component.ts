4bc370a7b74b48e3a341900d98b16f0757b611f8
Karyn Herrera <karynherrera@gmail.com>
2018-11-25 10:12:49 -0300

    actualiza mapa

--- src/app/sensores/acelerometro/acelerometro.component.ts
+++ src/app/sensores/acelerometro/acelerometro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Gyroscope, AbsoluteOrientationSensor, Accelerometer } from 'motion-sensors-polyfill';
+
 @Component({
 selector: 'app-acelerometro',
 templateUrl: './acelerometro.component.html',
@@ -15,17 +15,7 @@ constructor() {
   
 }
   ngOnInit() {
-    this.sensor = new Accelerometer();
-    this.sensor.start();
-    this.sensor.onreading = () => {
-      this.x = this.sensor.x;
-      this.y = this.sensor.y;
-      this.z = this.sensor.z;
-    console.log("Acceleration along X-axis: " + this.sensor.x);
-    console.log("Acceleration along Y-axis: " + this.sensor.y);
-    console.log("Acceleration along Z-axis: " + this.sensor.z);
-    }
-    this.sensor.onerror = event => console.log(event.error.name, event.error.message);
+    
   }
   
 }
